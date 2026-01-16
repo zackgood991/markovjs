264e486bc0c1cca2cbefc4ff68e40a45aa47afb4
Zack He <user@example.com>
2026-01-16 13:35:15 +1000

    Remove table logging as it gets messy and slow for bigger files

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ let table  =
     markov.generate_markov_table(
         "juicy fruit - it keeps on going. the endless " +
         "flavour, it'll keep you going. that's juicy j" +
-        "uicy juicy fruit! the flavour that judt keeps" +
+        "uicy juicy fruit! the flavour that just keeps" +
         " on going! juicy juicy juicy juicy fruit!"
     );
 
@@ -15,7 +15,6 @@ if(process.argv.length > 4) {
     fs.readFile(process.argv[2], "utf8", function(err, data) {
         if(err) throw err;
         table = markov.generate_markov_table(data, parseInt(process.argv[4]), true);
-        console.log(table);
 
         console.log(markov.generate_markov_text(parseInt(process.argv[3]), table, parseInt(process.argv[4])));
     });
