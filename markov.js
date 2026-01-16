757ac21d79099353563a703463389da37b0e919e
Zack He <user@example.com>
2026-01-16 13:30:29 +1000

    Markov per-word tokenizer/iser

--- markov.js
+++ markov.js
@@ -27,25 +27,35 @@
     OTHER DEALINGS IN THE SOFTWARE.
 */
 
+const markov_delim = " "; // word delimiter
+
 function generate_markov_table(text, look_forward = 4, active_log = false) {
     let table = {};
     let oldPer = 0;
+    let curMax; // current max of for loop
+
+    // if look forward is 0, tokenize by word
+    let textSplit = [];
+    if (look_forward == 0)
+        textSplit = text.split(markov_delim);
 
     // now walk through the text and make the index table
-    for (let i = 0; i < text.length; i++) {
-        const char = text.substring(i, i + look_forward);
-        table[char] = {};
+    curMax = look_forward == 0 ? textSplit.length : text.length;
+    for (let i = 0; i < curMax; i++) {
+        const char = look_forward == 0 ? textSplit[i] : text.substring(i, i + look_forward);
+        table[char] = {}; // if it overwrites anything, it doesn't matter
 
         if(!active_log) continue;
-        const curPer = Math.floor(50 * (i / (text.length == 0 ? 1 : text.length)));
+        const curPer = Math.floor(50 * i / (curMax == 0 ? 1 : curMax));
         if(oldPer != curPer) console.log(`Table: ${curPer}%`);
         oldPer = curPer;
     }
 
     // walk the array again and count the numbers
-    for (let i = 0; i < (text.length - look_forward); i++) {
-        const char_index = text.substring(i, i + look_forward);
-        const char_count = text.substring(i + look_forward, i + look_forward*2);
+    curMax = look_forward == 0 ? (textSplit.length - 1) : (text.length - look_forward);
+    for (let i = 0; i < curMax; i++) {
+        const char_index = look_forward == 0 ? textSplit[i    ] : text.substring(i, i + look_forward);
+        const char_count = look_forward == 0 ? textSplit[i + 1] :text.substring(i + look_forward, i + look_forward*2);
 
         if (Object.keys(table[char_index]).includes(char_count)) {
             table[char_index][char_count]++;
@@ -53,9 +63,9 @@ function generate_markov_table(text, look_forward = 4, active_log = false) {
             table[char_index][char_count] = 1;
         }
 
-        if(!active_log) continue;
-        const curPer = 50 + Math.floor(50 * (i / (text.length == 4 ? 1 : (text.length - look_forward))));
-        if(oldPer != curPer) console.log(`Table: ${curPer}%`);
+        if (!active_log) continue;
+        const curPer = 50 + Math.floor(50 * (i / (curMax == 0 ? 1 : curMax)));
+        if (oldPer != curPer) console.log(`Table: ${curPer}%`);
         oldPer = curPer;
     }
 
@@ -65,19 +75,27 @@ function generate_markov_table(text, look_forward = 4, active_log = false) {
 function generate_markov_text(length, table, look_forward = 4, active_log = false) {
     // get first character
     const tabKeys = Object.keys(table);
-    let char = tabKeys[Math.floor(Math.random()*tabKeys.length)];
+    let char = tabKeys[Math.floor(Math.random() * tabKeys.length)];
     let o = char;
 
-    for (let i = 0; i < (length / look_forward); i++) {
+    let i = 0;
+    while (look_forward == 0 ? o.length < length : i < (length / look_forward)) {
         const newchar = return_weighted_char(table[char]);
 
         if (newchar != null) {
             if (active_log && typeof process !== "undefined") process.stdout.write(newchar);
+            o   += newchar;
+
+            if (look_forward == 0)
+            if (!char.endsWith("\n")) // avoid unneeded extra condition check
+                o += markov_delim;
+
             char = newchar;
-            o += newchar;
         } else {
-            char = tabKeys[Math.floor(Math.random()*tabKeys.length)];
+            char = tabKeys[Math.floor(Math.random() * tabKeys.length)];
         }
+
+        if (look_forward != 0) i++;
     }
 
     return o;
