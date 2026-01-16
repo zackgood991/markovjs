b1fa9e222977a6a8b5ad43fb17ed7ac597686b4d
Zack He <user@example.com>
2026-01-16 10:00:11 +1000

    Add new example feature

--- example.js
+++ example.js
@@ -1,6 +1,7 @@
 const markov = require("./markov.js");
 
-const table  =
+console.log(process.argv);
+let table  =
     markov.generate_markov_table(
         "juicy fruit - it keeps on going. the endless " +
         "flavour, it'll keep you going. that's juicy j" +
@@ -8,5 +9,14 @@ const table  =
         " on going! juicy juicy juicy juicy fruit!"
     );
 
+if(process.argv.length > 4) {
+    const fs = require("fs");
 
-console.log(markov.generate_markov_text(200, table));
+    fs.readFile(process.argv[2], "utf8", function(err, data) {
+        if(err) throw err;
+        table = markov.generate_markov_table(data, parseInt(process.argv[4]), true);
+
+        console.log(markov.generate_markov_text(parseInt(process.argv[3]), table, parseInt(process.argv[4])));
+    });
+} else
+    console.log(markov.generate_markov_text(200, table));
