8453a540c8e69a03af9a35c3da8a10ed7c7a367a
Zack He <user@example.com>
2026-01-16 13:29:58 +1000

    Add table logging to example.js

--- example.js
+++ example.js
@@ -15,6 +15,7 @@ if(process.argv.length > 4) {
     fs.readFile(process.argv[2], "utf8", function(err, data) {
         if(err) throw err;
         table = markov.generate_markov_table(data, parseInt(process.argv[4]), true);
+        console.log(table);
 
         console.log(markov.generate_markov_text(parseInt(process.argv[3]), table, parseInt(process.argv[4])));
     });
